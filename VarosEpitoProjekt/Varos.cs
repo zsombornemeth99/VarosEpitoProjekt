71894ad03913116b325556dd223195c2fcc90d4a
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-10-29 11:54:30 +0100

    Add project files.

---
+++ VarosEpitoProjekt/Varos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarosEpitoProjekt
+{
+    class Varos
+    {
+        public string Nev { get; set; }
+        public int Lakosok { get; set; }
+        public int Hazak { get; set; }
+        public int Uzletek { get; set; }
+
+    }
+}
